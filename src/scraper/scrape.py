2db47777c36e44225f059662b888042edbb0e274
JvsonKee <jasonk2723@gmail.com>
2025-02-10 01:31:21 -0700

    scraper fully functional

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -1,64 +1,45 @@
+import util
+import database.database as db
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from util import scrape_ids
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains 
+from psycopg2.extras import DictCursor
 
 def scrape():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.page_load_strategy = 'eager'
+    driver = webdriver.Chrome(options=options)
+
     url = 'https://killerwhales.fandom.com/wiki/Category:Living_Wild_Orcas'
     driver.get(url)
 
-    scrape_ids(driver)
+    conn = None
+    cur = None
 
-scrape()
+    try:
+        conn = db.connect()
+        cur = conn.cursor(cursor_factory=DictCursor)
+
+        # reset db
+        db.reset_db(cur)
+
+        util.initialize_ids_and_names(driver, 1, cur)
+        
+        util.insert_granny(cur)
+
+        # retrieve whale information
+        whales = db.fetch_whales(cur)
+
+        # visit the corresponding whales' page and scrape their data
+        util.scrape_whales(driver, cur, whales)
+
+        conn.commit()
+
+    except Exception as error:
+        print(error)
+    finally:
+        db.close(conn, cur)
 
 
-
-
-# use this to get total number of sections on page
-# //*[@id="mw-content-text"]/div[3]
-
-# iterate through each section and only scrape the data for "J", "K", and "L"
-# //*[@id="mw-content-text"]/div[3]/div[index]/div
-
-# this contains the sections with each whale's name
-# //*[@id="mw-content-text"]/div[3]/div[7]/ul
-
-# extract the name of the whale
-# //*[@id="mw-content-text"]/div[3]/div[7]/ul/li[1]/a
-
-
-
-
-
-
-# jpod: /html/body/div[1]/main/article/section[3]/div[2]/div/div/div/div[5]/div/div/div/p[1]
-
-# kpod: /html/body/div[1]/main/article/section[10]/div[2]/div/div/div/div[5]/div/div/div/p[1]
-
-# lpod: /html/body/div[1]/main/article/section[15]/div[2]/div/div/div/div[5]/div/div/div/p[1]
-
-# pod section ids: 
-#    3 (j pod) 
-#   10 (k pod)
-#   15 (l pod)
-
-# use pod section to get total number of sections
-# pod section: /html/body/div[1]/main/article/section[{section-id}]/div[2]/div/div/div
-
-# iterate from 6th index to total number of sections
-# family: /html/body/div[1]/main/article/section[{section-id}]/div[2]/div/div/div/div[{n}]/div/div/div/p
-
-# each family text is going to return a string with line breaks
-# split the string by line breaks 
-# the first element is the family id
-# the following are the members of the family
-
-# from each family member we can extract the following:
-    # id
-    # nickname
-    # sex
-    # birth year
-
-    # format: "id nickname (sex, born year)"
-
-# create an object (dictionary) with the extracted data
+if __name__ == '__main__':
+    scrape()
--- src/scraper/util.py
+++ src/scraper/util.py
@@ -1,117 +1,190 @@
+from database.database import insert_whale, insert_pod
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import db.db as db
+from selenium.webdriver.common.action_chains import ActionChains 
+import re
 
-def scrape_ids(driver):
-    living_sections_len = len(driver.find_elements(By.XPATH, '//*[@id="mw-content-text"]/div[3]/div'))
+def initialize_ids_and_names(driver, n, cur):
+    sections_len = len(driver.find_elements(By.XPATH, '//*[@id="mw-content-text"]/div[3]/div'))
     
     pod_indices = []
     pod_counts = []
 
-    for i in range(1, living_sections_len):
+    # insert relevant pods into db and get their indices for scraping
+    for i in range(1, sections_len + 1):
         curr_letter = driver.find_element(By.XPATH, f'//*[@id="mw-content-text"]/div[3]/div[{i}]/div').text
         
         if curr_letter == 'J' or curr_letter == 'K' or curr_letter == 'L':
-            pod_indices.append(i)
-            pod_counts.append(len(driver.find_elements(By.XPATH, f'//*[@id="mw-content-text"]/div[3]/div[{i}]/ul/li')))
+            pod = (curr_letter, f'{curr_letter} Pod')
+            insert_pod(cur, pod)
 
+            pod_indices.append(i)
+            pod_counts.append(len(driver.find_elements(By.XPATH, f'//*[@id="mw-content-text"]/div[3]/div[{i}]/ul/li')) + 1)
+
+    # iterate through pod list, parse whale information, and insert to db
     for i in range(len(pod_indices)):
         for j in range(1, pod_counts[i]):
             id_and_name = driver.find_element(By.XPATH, f'//*[@id="mw-content-text"]/div[3]/div[{pod_indices[i]}]/ul/li[{j}]/a').text
             
-            parts = id_and_name.split(" ")
+            parts = id_and_name.split(" ", 1)
+            
+            whale_id = parts[0]
+            whale = None
+            pod = whale_id[0]
 
             if (len(parts) < 2):
-                continue
+                if whale_id[-1].isdigit():
+                    whale = (whale_id, None, None, None, None, None, None, pod)
+                else:
+                    continue
+            else:
+                name = parts[1]
+                whale = (whale_id, name, None, None, None, None, None, pod)
+            
+            insert_whale(cur, whale)
 
-            whale_id = parts[0]
-            name = parts[1]
+    # navigate to deceased page and parse whale data
+    if n > 0:
+        try:
+            navigate_menu(driver, 2)
+            initialize_ids_and_names(driver, n - 1, cur)
+        except NoSuchElementException:
+            return
+        
 
-            whale = (whale_id, name, None, None, None)
-            db.insert_whale(whale)
+# menu navigation function
+# index = 1: living
+# index = 2: deceased
+def navigate_menu(driver, index):
+    menu = driver.find_element(By.XPATH, '/html/body/div[4]/div[4]/div[1]/header/nav/ul/li[3]/div[1]/a')
 
-def get_section_limit(driver, section_id):
-    section_container = driver.find_element(By.XPATH, f'/html/body/div[1]/main/article/section[{section_id}]/div[2]/div/div/div')
-    return len(section_container.find_elements(By.XPATH, 'div'))
+    action = ActionChains(driver)
+    action.move_to_element(menu).perform()
 
-def scrape_pods(driver):
-    pod_ids = [3, 10, 15]
-    pods = []
-
-    for pod_id in pod_ids:
-        pods.append(scrape_pod(driver, pod_id))
-
-    return pods
-
-def scrape_pod(driver, section_id):
-
-    name = "J Pod" if section_id == 3 else "K Pod" if section_id == 10 else "L Pod"
-
-    families = scrape_families(driver, section_id)
-    members = []
-
-    for family in families:
-        for member in family['family_members']:
-            if member['id'] not in [member['id'] for member in members]:
-                members.append(member)
-
-    pod = {
-        'name': name,
-        'matrilines': [],
-        'families': families,
-        'members': members
-    }
-
-    return pod
-
-def scrape_families(driver, section_id):
-    families = []
-
-    for i in range(6, get_section_limit(driver, section_id)):
-        scrape_family(driver, section_id, i)
-        family = scrape_family(driver, section_id, i)
-        families.append(family)
-
-    return families
+    driver.find_element(By.XPATH, f'/html/body/div[4]/div[4]/div[1]/header/nav/ul/li[3]/div[2]/ul/li[{index}]/a').click()
 
 
-def scrape_family(driver, section_id, index):
-    family_string = driver.find_element(By.XPATH, f'/html/body/div[1]/main/article/section[{section_id}]/div[2]/div/div/div/div[{index}]/div/div/div/p').text
-    parts = family_string.splitlines()
+# visit each whales page and scrape the relevant data
+def scrape_whales(driver, cur, whales):
+    for whale in whales:
+        whale_id = whale["whale_id"]
+        path = ""
 
-    family_id = parts[0]
-    members = []
+        if (whale["name"] is not None):
+            name = whale["name"].replace(" ", "_").replace("'", "%27")
+            path = f'{whale["whale_id"]}_{name}'
+        else:
+            path = f'{whale["whale_id"]}'
 
-    for member in parts[1:]:
-        members.append(scrape_member(member))
+        driver.get(f'https://killerwhales.fandom.com/wiki/{path}')
+        # print(path)
+        
+        # scrape data here (status, gender, dob, dod, mother, father)
+        scrape_whale(driver, cur, whale_id)
 
-    family = {
-        'id': family_id,
-        'matriarch': '',
-        'family_members': members
-    }
 
-    return family
+def scrape_whale(driver, cur, whale_id):
+    gender = None
+    birth_year = None
+    death_year = None
+    mother_id = None
+    father_id = None
 
-def scrape_member(member_string):
-    id_and_nickname, bracketed = member_string.split("(", 1)
-    id_and_nickname = id_and_nickname.strip()
-    bracketed = bracketed.rstrip(")")
+    query = """ 
+                UPDATE whale 
+                SET gender = %s, birth_year = %s, death_year = %s, mother_id = %s, father_id = %s
+                WHERE whale_id = %s;             
+            """
 
-    if (len(id_and_nickname.split()) < 2):
-        member_id = id_and_nickname
-        nickname = None
-    else:
-        member_id, nickname = id_and_nickname.split(" ", 1)
+    gender_sources = [
+                        '//*[@id="mw-content-text"]/div/aside/div[2]/div',
+                        '//*[@id="mw-content-text"]/div/aside/div[3]/div',
+                        '//*[@id="mw-content-text"]/div/aside/div[4]/div',
+                        '//*[@id="mw-content-text"]/div/aside/div[4]/div/i'
+                    ]
     
-    parts = bracketed.split(", ")
+    for source in gender_sources:
+        gender = driver.find_element(By.XPATH, f'{source}').text.split(" ", 1)[-1].lower()
 
-    member = {
-        'id': member_id,
-        'nickname': nickname,
-        'sex': parts[0],
-        'birth_year': int(parts[1][-4:]),
-        'pod': member_id[0]
-    }
+        if gender in {"unknown", "male", "female"}:
+            break
 
-    return member
+    birth_sources = [
+        '//*[@id="mw-content-text"]/div/aside/div[3]/div',
+        '//*[@id="mw-content-text"]/div/aside/div[4]/div',
+        '//*[@id="mw-content-text"]/div/aside/div[5]/div',
+    ]
+
+    for source in birth_sources:
+        birth_text = driver.find_element(By.XPATH, f'{source}').text
+        birth_year = parse_year(birth_text)
+
+        if isinstance(birth_year, int):
+            break
+         
+    death_sources = [
+        '//*[@id="mw-content-text"]/div/aside/div[4]',
+        '//*[@id="mw-content-text"]/div/aside/div[6]',
+        '//*[@id="mw-content-text"]/div/aside/div[9]',
+        '//*[@id="mw-content-text"]/div/aside/div[5]',
+    ]
+
+    for source in death_sources:
+        try:
+            death_elem = driver.find_element(By.XPATH, f'{source}/h3')
+
+            if death_elem.text == "Date of Death":
+                death_text = driver.find_element(By.XPATH, f'{source}/div').text
+                death_year = parse_year(death_text)
+
+            if isinstance(death_year, int):
+                break
+
+        except NoSuchElementException:
+            death_year = None
+
+
+    try:
+        mother_elem = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div/aside/section[2]/div/h3 | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/h3')
+        if mother_elem.text == "Mother":
+            mother_text = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/ul/li/a/u/i/b | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/a/u/b | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/ul/li/a/u/b | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/a/u/i/b | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/a/b/u | //*[@id="mw-content-text"]/div/aside/section[2]/div[1]/div/b/u/a').text
+            mother_id = mother_text.split(" ")[0]
+
+    except NoSuchElementException:
+        mother_id = None
+
+    try:
+        father_elem = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div/aside/section[2]/div[2]/h3')
+        if father_elem.text == "Father":
+            father_text = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div/aside/section[2]/div[2]/div/a | //*[@id="mw-content-text"]/div/aside/section[2]/div[2]/div/i/a').text
+            father_id = father_text.split(" ")[0]
+
+    except NoSuchElementException:
+        father_id = None
+
+    cur.execute(query, (gender, birth_year, death_year, mother_id, father_id, whale_id))
+
+
+def parse_year(text):
+    match = re.search(r'\b(18[0-9]{2}|19[0-9]{2}|20[0-2][0-9])\b', text)
+    return int(match.group()) if match else None
+
+
+def insert_granny(cur):
+    whale_id = 'J2'
+    name = 'Granny'
+    gender = 'female'
+    birth_year = 1936
+    death_year = 2016
+    mother_id = None
+    father_id = None
+    pod_id = 'J'
+
+    granny = (whale_id, name, gender, birth_year, death_year, mother_id, father_id, pod_id)
+
+    query = """
+                INSERT INTO whale (whale_id, name, gender, birth_year, death_year, mother_id, father_id, pod_id) VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (whale_id) DO NOTHING;
+            """
+    
+    cur.execute(query, granny)
