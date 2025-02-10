be94a97f996eafa89af52e30dc7ce23cf692d28d
JvsonKee <jasonk2723@gmail.com>
2025-02-10 06:07:41 -0700

    bug fixes and api route to get pods

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,34 @@
 import database as db
 from flask import Flask, jsonify
 from flask_cors import CORS
-from psycopg2.extras import DictCursor
 
 
 app = Flask(__name__)
 CORS(app)
 
+# route to get all pods
+@app.route('/pods', methods=['GET'])
+def get_pods():
+    conn = db.connect()
+    cur = conn.cursor()
+
+    pods = db.fetch_pods(cur)
+
+    db.close(conn, cur)
+
+    pod_list = [{
+        'pod_id': pod[0],
+        'name': pod[1]
+    } for pod in pods]
+
+    return jsonify(pod_list)
+
+
 # route to get all the whales
 @app.route('/whales', methods=['GET'])
 def get_whales():
     conn = db.connect()
-    cur = conn.cursor(cursor_factory=DictCursor)
+    cur = conn.cursor()
 
     whales = db.fetch_whales(cur)
 
@@ -20,11 +37,12 @@ def get_whales():
     whale_list = [{
         'whale_id': whale[0],
         'name': whale[1],
-        'gender': whale[2],
+        'gender': whale[2].strip(),
         'birth_year': whale[3],
         'death_year': whale[4],
-        'father_id': whale[5],
-        'pod_id': whale[6]
+        'mother_id': whale[5],
+        'father_id': whale[6],
+        'pod_id': whale[7],
     } for whale in whales]
 
     return jsonify(whale_list)
