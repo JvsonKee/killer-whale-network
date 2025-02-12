6d28e566bd74be83feb2f55a90c4f35998dc6a81
JvsonKee <jasonk2723@gmail.com>
2025-02-12 09:57:25 -0700

    error handling and route updates

--- backend/server.py
+++ backend/server.py
@@ -11,6 +11,9 @@ CORS(app)
 def get_pods():
     pods = fetch_process(db.fetch_pods)
 
+    if not pods:
+        return jsonify({"error": f"No pods founds"}), 404
+
     pod_list = [{
         'pod_id': pod[0],
         'name': pod[1]
@@ -19,17 +22,37 @@ def get_pods():
     return jsonify(pod_list)
 
 
-# gets whales from a certain pod
-@app.route('/whales/<pod_id>', methods={'GET'})
-def get_whales_from_pod(pod_id):
-    whales = fetch_process(db.fetch_whales_from_pod, pod_id.upper())
-    return jsonify_whales(whales)
-
-
 # gets all whales
 @app.route('/whales', methods=['GET'])
 def get_whales():
     whales = fetch_process(db.fetch_whales)
+    return jsonify_whales(whales)
+
+
+@app.route('/whales/<whale_id>', methods=['GET'])
+def get_whale(whale_id):
+    whale = fetch_process(db.fetch_whale, whale_id)
+
+    if not whale:
+        return jsonify({"error": f"No whale found with id: {whale_id}"}), 404
+    
+    whale = {
+        'whale_id': whale[0],
+        'name': whale[1],
+        'gender': whale[2].strip(),
+        'birth_year': whale[3],
+        'death_year': whale[4],
+        'mother_id': whale[5],
+        'father_id': whale[6],
+        'pod_id': whale[7],
+    }
+    return jsonify(whale)
+
+
+# gets whales from a certain pod
+@app.route('/whales/pod/<pod_id>', methods=['GET'])
+def get_whales_from_pod(pod_id):
+    whales = fetch_process(db.fetch_whales_from_pod, pod_id.upper())
     return jsonify_whales(whales)
 
 
@@ -48,21 +71,21 @@ def get_deceased_whales():
 
 
 # gets all living whales from a pod
-@app.route('/whales/living/<pod_id>', methods=['GET'])
+@app.route('/whales/living/pod/<pod_id>', methods=['GET'])
 def get_living_from_pod(pod_id):
     whales = fetch_process(db.fetch_living_from_pod, pod_id.upper())
     return jsonify_whales(whales)
 
 
 # gets all deceased whales from a pod
-@app.route('/whales/deceased/<pod_id>', methods=['GET'])
+@app.route('/whales/deceased/pod/<pod_id>', methods=['GET'])
 def get_deceased_from_pod(pod_id):
     whales = fetch_process(db.fetch_deceased_from_pod, pod_id.upper())
     return jsonify_whales(whales)
 
 
 # gets all children of parent
-@app.route('/<parent_id>/children', methods=['GET'])
+@app.route('/whales/<parent_id>/children', methods=['GET'])
 def get_children(parent_id):
     whales = fetch_process(db.fetch_children, parent_id)
     return jsonify_whales(whales)
@@ -74,15 +97,22 @@ def fetch_process(func, arg=None):
     conn = db.connect()
     cur = conn.cursor()
 
-    items = func(cur) if arg is None else func(cur, arg)
+    result = func(cur) if arg is None else func(cur, arg)
 
     db.close(conn, cur)
 
-    return items
+    if not result:
+        return None
+
+    return result
 
 
 # helper function to jsonify whale array
-def jsonify_whales(whales):
+def jsonify_whales(response):
+
+    if response == None:
+        return jsonify({"error": "No items found."}), 404
+
     whale_list = [{
         'whale_id': whale[0],
         'name': whale[1],
@@ -92,7 +122,7 @@ def jsonify_whales(whales):
         'mother_id': whale[5],
         'father_id': whale[6],
         'pod_id': whale[7],
-    } for whale in whales]
+    } for whale in response]
 
     return jsonify(whale_list)
 
