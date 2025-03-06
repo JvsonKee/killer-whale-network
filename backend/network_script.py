2afac7cdd5c260dd3ac4eaafa85c23a9ee0d5b3a
JvsonKee <jasonk2723@gmail.com>
2025-03-06 13:07:49 -0700

    creating edge list from csv

--- backend/network_script.py
+++ backend/network_script.py
@@ -2,19 +2,28 @@ import pandas as pd
 
 whale_df = pd.read_csv('./data/srkw.csv')
 
-edges = []
+def jsonify_whales():
+    nodes = whale_df.rename(columns={'whale_id': 'id'})
+    json_data = nodes.to_json(orient='records')
 
-for _, row in whale_df.iterrows():
-    if pd.notnull(row['mother_id']):
-        edges.append((row['mother_id'], row['whale_id'],))
+    return json_data
 
-    if pd.notnull(row['father_id']):
-        edges.append((row['father_id'], row['whale_id'],))
 
-edges_df = pd.DataFrame(list(edges), columns=['Source', 'Target'])
+def jsonify_edge_list():
+    edges = create_edge_list()
+    json_data = edges.to_json(orient='records')
 
-edges_df.to_csv('./data/edge_list.csv', index=False)
+    return json_data
 
-whale_df = whale_df.rename(columns={'whale_id': 'Id', 'name': 'Label'})
 
-whale_df.to_csv('./data/srkw_g.csv', index=False)
+def create_edge_list():
+    edges = []
+
+    for _, row in whale_df.iterrows():
+        if pd.notnull(row['mother_id']):
+            edges.append((row['mother_id'], row['whale_id'],))
+
+        if pd.notnull(row['father_id']):
+            edges.append((row['father_id'], row['whale_id'],))
+
+    return pd.DataFrame(list(edges), columns=['source', 'target'])
