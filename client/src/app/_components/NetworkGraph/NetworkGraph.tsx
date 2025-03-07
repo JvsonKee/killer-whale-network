6185968e374be573c000753f9f9bbadc147bba4f
JvsonKee <jasonk2723@gmail.com>
2025-03-07 00:24:30 -0700

    sizing updates for larger graphs

--- client/src/app/_components/NetworkGraph/NetworkGraph.tsx
+++ client/src/app/_components/NetworkGraph/NetworkGraph.tsx
@@ -12,8 +12,10 @@ interface NetworkGraphProps {
 }
 
 export default function NetworkGraph({ data } : NetworkGraphProps) {
-    const filter : string = 'gender';
+    const filter : string = 'pod';
     const svgRef = useRef<SVGSVGElement>(null);
+    const simulationRef = useRef<d3.Simulation<Node, Link> | null>(null);
+
 
     const genderColors = d3.scaleOrdinal<string>()
         .domain(['male', 'female', 'unknown'])
@@ -24,16 +26,27 @@ export default function NetworkGraph({ data } : NetworkGraphProps) {
         .range(['#cab6ff', '#ffb297', '#addaff'])
 
     useEffect(() => {
-        const whales = data.whales.map(d => ({...d}));
-        const links = data.links.map(d => ({...d}));
+        if (simulationRef.current) {
+            simulationRef.current.stop();
+        }
+
+        const { whales, links } = data;
+
+        const manyNodes = whales.length > 99;
+
+        console.log(whales.length)
 
         const width = 1400;
-        const height = 1200;
+        const height = 1000;
 
         const svg = d3
             .select(svgRef.current)
             .attr('width', width)
             .attr('height', height);
+
+        svg.selectAll('*').remove();
+
+        console.log(links)
 
         svg.append('defs')
             .selectAll('marker')
@@ -41,30 +54,34 @@ export default function NetworkGraph({ data } : NetworkGraphProps) {
             .join('marker')
             .attr('id', (_, i) => `arrowhead-${i}`)
             .attr('viewBox', '0 -5 10 10')
-            .attr('refX', 9) // space from the arrow to the end of the link
+            .attr('refX', 9)
             .attr('refY', 0)
             .attr('orient', 'auto')
-            .attr('markerWidth', 6)
-            .attr('markerHeight', 8)
+            .attr('markerWidth', manyNodes ? 6 : 4)
+            .attr('markerHeight', manyNodes ? 8 : 6)
             .append('path')
-            .attr('d', 'M0,-5L10,0L0,5') // Create the arrow shape
+            .attr('d', 'M0,-5L10,0L0,5')
             .attr('fill', d => {
                 const node = whales.find(whale => whale.whale_id === d.target);
                 return getNodeColor(node, filter);
             });
 
+        const forceStrength = manyNodes ? -100 : -250;
+
         const simulation = d3.forceSimulation<Node>(whales)
-            .force('link', d3.forceLink<Node, Link>(links).id(d => d.whale_id).distance(50))
-            .force('charge', d3.forceManyBody().strength(-100))
+            .force('link', d3.forceLink<Node, Link>(links).id(d => d.whale_id).distance(70))
+            .force('charge', d3.forceManyBody().strength(forceStrength))
             .force("x", d3.forceX(width / 2))
             .force("y", d3.forceY(height / 2));
 
+        simulationRef.current = simulation;
+
         const link = svg.append('g')
-            .attr('stroke-width', 1)
+            .attr('stroke-width', manyNodes ? 1 : 1.75)
             .attr('opacity', 0.6)
             .attr('fill', 'none')
             .selectAll<SVGPathElement, Link>('path')
-            .data(links)
+            .data(links, d => d.target.whale_id)
             .join('path')
             .attr('stroke', d => {
                 const targetId = typeof d.target === 'string' ? d.target : d.target.whale_id;
@@ -73,28 +90,37 @@ export default function NetworkGraph({ data } : NetworkGraphProps) {
             })
             .attr('marker-end', (_, i) => `url(#arrowhead-${i})`);
 
-        const node = svg.append('g')
-            .attr('stroke', '#fff')
-            .attr('stroke-width', 1.5)
-            .selectAll<SVGCircleElement, Node>('circle')
+        const nodeGroup = svg.append('g')
+            .selectAll<SVGElement, Node>('g')
             .data(whales, d => d.whale_id)
-            .join('circle')
-            .attr('r', 8)
+            .join('g')
+            .call(drag(simulation));
+
+        nodeGroup.append('circle')
+            .attr('r', manyNodes ? 9 : 15)
             .attr('fill', d => getNodeColor(d, filter))
             .attr('fill-opacity', 0.7)
             .attr('stroke', d => getNodeColor(d, filter))
             .attr('stroke-width', 3)
-            .call(drag(simulation));
+            .attr('opacity', d => d.death_year ? 0.3 : 1);
 
-        const text = svg.append('g')
-            .selectAll<SVGTextElement, Node>('text')
-            .data(whales, d => d.whale_id)
-            .join('text')
+        nodeGroup.append('text')
             .text(d => d.whale_id)
-            .attr('font-size', 12)
-            .attr('fill', "white")
-            .attr('dx', 14)
-            .attr('dy', '.35em');
+            .attr('font-size', manyNodes ? 8 : 10)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .attr('dy', 2.5)
+            .style('pointer-events', 'none');
+
+        if (whales.length < 120) {
+            nodeGroup.append('text')
+                .text(d => d.name)
+                .attr('font-size', 10)
+                .attr('fill', 'white')
+                .attr('x', 19)
+                .attr('dy', 2.5)
+                .style('pointer-events', 'none');
+        }
 
         simulation.on('tick', () => {
             link.attr('d', d => {
@@ -111,12 +137,12 @@ export default function NetworkGraph({ data } : NetworkGraphProps) {
                 const dx = targetX - sourceX;
                 const dy = targetY - sourceY;
                 const normal = { x: -dy, y: dx }; // perpendicular direction
-                const curveStrength = 3; // play with this number
+                const curveStrength = manyNodes ? 5 : 3; // play with this number
                 const normalLength = Math.sqrt(normal.x ** 2 + normal.y ** 2);
                 const offsetX = midX + (normal.x / normalLength) * curveStrength;
                 const offsetY = midY + (normal.y / normalLength) * curveStrength;
 
-                const gap = 12; // The gap size
+                const gap = manyNodes ? 12 : 20; // The gap size
                 const gapOffsetX1 = sourceX + (dx / Math.sqrt(dx * dx + dy * dy)) * gap;
                 const gapOffsetY1 = sourceY + (dy / Math.sqrt(dx * dx + dy * dy)) * gap;
                 const gapOffsetX2 = targetX - (dx / Math.sqrt(dx * dx + dy * dy)) * gap;
@@ -125,34 +151,28 @@ export default function NetworkGraph({ data } : NetworkGraphProps) {
                 return `M${gapOffsetX1},${gapOffsetY1} Q${offsetX},${offsetY} ${gapOffsetX2},${gapOffsetY2}`;
             });
 
-            node
-                .attr("cx", d => (d as Node).x!)
-                .attr("cy", d => (d as Node).y!);
-
-            text
-                .attr('x', d => (d as Node).x!)
-                .attr('y', d => (d as Node).y!);
+            nodeGroup.attr('transform', d => `translate(${(d as Node).x},${(d as Node).y})`);
         });
 
         function drag(simulation: d3.Simulation<Node, Link>) {
-            function dragstarted(event: d3.D3DragEvent<SVGCircleElement, Node, unknown>, d: Node) {
+            function dragstarted(event: d3.D3DragEvent<SVGElement, Node, unknown>, d: Node) {
                 if (!event.active) simulation.alphaTarget(0.3).restart();
                     d.fx = d.x;
                     d.fy = d.y;
                 }
 
-            function dragged(event: d3.D3DragEvent<SVGCircleElement, Node, unknown>, d: Node) {
+            function dragged(event: d3.D3DragEvent<SVGElement, Node, unknown>, d: Node) {
                 d.fx = event.x;
                 d.fy = event.y;
             }
 
-            function dragended(event: d3.D3DragEvent<SVGCircleElement, Node, unknown>, d: Node) {
+            function dragended(event: d3.D3DragEvent<SVGElement, Node, unknown>, d: Node) {
                 if (!event.active) simulation.alphaTarget(0);
                     d.fx = null;
                     d.fy = null;
                 }
 
-            return d3.drag<SVGCircleElement, Node>()
+            return d3.drag<SVGElement, Node>()
                 .on('start', dragstarted)
                 .on('drag', dragged)
                 .on('end', dragended);
