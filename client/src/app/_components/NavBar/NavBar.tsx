d2b1e121b39196e1fb605fcf66fa20bcab803dba
JvsonKee <jasonk2723@gmail.com>
2025-03-08 10:21:07 -0700

    nav bar styling

--- client/src/app/_components/NavBar/NavBar.tsx
+++ client/src/app/_components/NavBar/NavBar.tsx
@@ -1,11 +1,53 @@
+'use client';
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function NavBar() {
+    const pages = [
+        {
+            name: 'Home',
+            path: '/'
+        },
+        {
+            name: 'About',
+            path: '/about'
+        },
+        {
+            name: 'Whales',
+            path: '/whales'
+        },
+        {
+            name: 'Threats & Conservation',
+            path: '/tc'
+        },
+    ]
+
     return (
-        <div>
-            <Link href="/">Home</Link>
-            <Link href="/whales">Whales</Link>
-            <Link href="/pods">Pods</Link>
+        <div className="flex items-center justify-center gap-20 h-[10vh]">
+            {
+                pages.map((page) => (
+                    <NavLink page={page} key={page.name}/>
+                ))
+            }
         </div>
+    )
+}
+
+interface NavLinkProps {
+    page: {
+        name: string, 
+        path: string
+    }
+}
+
+function NavLink({ page } : NavLinkProps) {
+    const pathName = usePathname();
+    const isActive = pathName === page.path;
+
+    return (
+        <Link href={`${page.path}`} className={`${isActive ? 'text-blue' : 'text-white'} hover:text-blue duration-300 ease-in-out`}>
+            { page.name }
+        </Link>
     )
 }
