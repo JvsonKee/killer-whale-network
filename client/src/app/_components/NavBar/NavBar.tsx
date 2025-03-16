4581a0209e0a6e0b658759a10af1f50b08fb61bf
JvsonKee <jasonk2723@gmail.com>
2025-03-15 23:13:51 -0600

    formatting

--- client/src/app/_components/NavBar/NavBar.tsx
+++ client/src/app/_components/NavBar/NavBar.tsx
@@ -1,53 +1,54 @@
-'use client';
+"use client";
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function NavBar() {
-    const pages = [
-        {
-            name: 'Home',
-            path: '/'
-        },
-        {
-            name: 'About',
-            path: '/about'
-        },
-        {
-            name: 'Whales',
-            path: '/whales'
-        },
-        {
-            name: 'Threats & Conservation',
-            path: '/tc'
-        },
-    ]
+  const pages = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Whales",
+      path: "/whales",
+    },
+    {
+      name: "Threats & Conservation",
+      path: "/threats-and-conservation",
+    },
+  ];
 
-    return (
-        <div className="flex items-center justify-center gap-20 h-[10vh]">
-            {
-                pages.map((page) => (
-                    <NavLink page={page} key={page.name}/>
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center gap-20 h-[10vh]">
+      {pages.map((page) => (
+        <NavLink page={page} key={page.name} />
+      ))}
+    </div>
+  );
 }
 
 interface NavLinkProps {
-    page: {
-        name: string, 
-        path: string
-    }
+  page: {
+    name: string;
+    path: string;
+  };
 }
 
-function NavLink({ page } : NavLinkProps) {
-    const pathName = usePathname();
-    const isActive = pathName === page.path;
+function NavLink({ page }: NavLinkProps) {
+  const pathName = usePathname();
+  const isActive = pathName === page.path;
 
-    return (
-        <Link href={`${page.path}`} className={`${isActive ? 'text-blue' : 'text-white'} hover:text-blue duration-300 ease-in-out`}>
-            { page.name }
-        </Link>
-    )
+  return (
+    <Link
+      href={`${page.path}`}
+      className={`${isActive ? "text-blue" : "text-white"} hover:text-blue duration-300 ease-in-out`}
+    >
+      {page.name}
+    </Link>
+  );
 }
