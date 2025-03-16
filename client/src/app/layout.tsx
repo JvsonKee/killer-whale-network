25d2cf18e191d17709bc173f9742d612a923a592
JvsonKee <jasonk2723@gmail.com>
2025-03-15 23:15:26 -0600

    site metadata

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,28 +1,31 @@
 import NavBar from "./_components/NavBar/NavBar";
-import { Inter } from 'next/font/google';
-import './globals.css';
+import Footer from "./_components/Footer/Footer";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 export const metadata = {
-  title: 'SRKW',
-  description: '',
-}
+  title: "Resident Connections",
+  description:
+    "Explore the world of the Southern Resident Killer Whales through interactive network graphs, in-depth pod histories, and conservation insights. Learn about J, K, and L pods, the threats they face, and how you can help protect these endangered killer whales.",
+};
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap'
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={inter.className}>
       <body>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
